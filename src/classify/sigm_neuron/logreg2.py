9f7f1d52f3585f8e3fbb6f1249dc7ec9ed913835
Valerii Zuev <valera.zuev.zva@gmail.com>
2019-11-03 18:37:43 +0300

    logistic regression: python: tune gradient descent to acceptable performance

--- src/classify/sigm_neuron/logreg2.py
+++ src/classify/sigm_neuron/logreg2.py
@@ -1,5 +1,5 @@
 from mathfunctions import *
-
+import sys
 
 def getxy(filename):
     delimiter = ','
@@ -16,19 +16,6 @@ def getxy(filename):
         row.insert(0, bias)
     return x_mtx, y_vec
 
-def normalize(x_mtx):
-    xmin = float('inf')
-    xmax = -float('inf')
-    for row in x_mtx:
-        for elem in row[1:]:  # exclude bias term
-            xmin = elem if elem < xmin else xmin
-            xmax = elem if elem > xmax else xmax
-    maxdiff = xmax - xmin
-    mean = 0.5*(xmax+xmin)
-    for row in x_mtx:
-        for irow in range(1,len(row)):
-            row[irow] -= mean
-            row[irow] /= maxdiff
 
 def cost_and_grad(x_mtx, y_vec, theta_vec):
     train_size = len(y_vec)
@@ -48,25 +35,27 @@ def cost_and_grad(x_mtx, y_vec, theta_vec):
     return cost, grad_vec
 
 
-def gradient_descent(x_mtx, y_vec, theta_vec, learn_rate=0.001, num_iter=500):
+def gradient_descent(x_mtx, y_vec, theta_vec, learn_rate=0.001, num_iter=100000):
     for it in range(num_iter):
         cost, grad_vec = cost_and_grad(x_mtx, y_vec, theta_vec)
         for i in range(len(theta_vec)):
             theta_vec[i] -= grad_vec[i] * learn_rate
+        if not it % 500:
+            sys.stdout.write('\r cost:' + str(round(cost, 4)) + ', iteration: ' + str(it))
+        print('')
     return theta_vec, cost
-
 
 
 if __name__ == '__main__':
     input_filename = 'matlab/data.txt'
     x_mtx, y_vec = getxy(input_filename)
-    normalize(x_mtx)
-    print(x_mtx)
     nparams = len(x_mtx[0])
     init_theta = [0 for _ in range(nparams)]
     # init_theta = [-25, 0.2, 0.2] # nearly the right answer
     init_cost, init_grad = cost_and_grad(x_mtx, y_vec, init_theta)
     print(init_cost)
+    print('running gradient descent...')
     res_theta, res_cost = gradient_descent(x_mtx, y_vec, init_theta)
-    print(res_cost)
-    print(res_theta)
+    print('model trained!')
+    print('cost: '+ str(res_cost))
+    print('theta: ' + str(res_theta))
