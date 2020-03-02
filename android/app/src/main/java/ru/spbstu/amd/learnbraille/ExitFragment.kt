50ec01046a3208056b089241f5022dbf4e476e61
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-03-02 08:12:47 +0300

    release0.1: add logic in exit fragment

--- android/app/src/main/java/ru/spbstu/amd/learnbraille/ExitFragment.kt
+++ android/app/src/main/java/ru/spbstu/amd/learnbraille/ExitFragment.kt
@@ -3,12 +3,19 @@ package ru.spbstu.amd.learnbraille
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlin.system.exitProcess
 
 class ExitFragment : Fragment() {
 
-    // TODO implement if needed
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.fragment_exit, container, false)
+    ) = inflater.inflate(R.layout.fragment_exit, container, false).apply{
+        val exitButton: Button = findViewById(R.id.exit_button)
+        exitButton.setOnClickListener{
+            exitProcess(0)
+        }
+    }
 }
