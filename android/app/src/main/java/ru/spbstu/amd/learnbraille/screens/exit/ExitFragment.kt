3d875e38e065cc651ad9f53ea0e5d86b1ab6f5bd
Мария <masha-bavikina@yandex.ru>
2020-03-14 16:40:16 +0300

    2.2 change behavior clicking no_button in exit fragment

--- android/app/src/main/java/ru/spbstu/amd/learnbraille/screens/exit/ExitFragment.kt
+++ android/app/src/main/java/ru/spbstu/amd/learnbraille/screens/exit/ExitFragment.kt
@@ -2,9 +2,11 @@ package ru.spbstu.amd.learnbraille.screens.exit
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import ru.spbstu.amd.learnbraille.R
 import ru.spbstu.amd.learnbraille.databinding.FragmentExitBinding
 import kotlin.system.exitProcess
@@ -23,6 +25,9 @@ class ExitFragment : Fragment() {
     ).apply {
         exitButton.setOnClickListener {
             exitProcess(0)
+        }
+        continueButton.setOnClickListener {view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_exitFragment_to_menuFragment)
         }
     }.root
 }
