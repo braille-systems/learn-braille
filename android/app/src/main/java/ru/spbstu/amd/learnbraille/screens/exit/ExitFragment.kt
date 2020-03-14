89e8f0295ada5f520a8a7903e9f289bf4b3c7c20
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-03-14 17:30:08 +0300

    Refactor style

--- android/app/src/main/java/ru/spbstu/amd/learnbraille/screens/exit/ExitFragment.kt
+++ android/app/src/main/java/ru/spbstu/amd/learnbraille/screens/exit/ExitFragment.kt
@@ -2,7 +2,6 @@ package ru.spbstu.amd.learnbraille.screens.exit
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -26,8 +25,8 @@ class ExitFragment : Fragment() {
         exitButton.setOnClickListener {
             exitProcess(0)
         }
-        continueButton.setOnClickListener {view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_exitFragment_to_menuFragment)
-        }
+        continueButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_exitFragment_to_menuFragment)
+        )
     }.root
 }
