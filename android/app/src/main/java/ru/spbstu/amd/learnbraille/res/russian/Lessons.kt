414827b9c7be7a8b791e741be14b047a6d666870
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-03-22 10:21:46 +0300

    3.6 correct lessons titles

--- android/app/src/main/java/ru/spbstu/amd/learnbraille/res/russian/Lessons.kt
+++ android/app/src/main/java/ru/spbstu/amd/learnbraille/res/russian/Lessons.kt
@@ -4,6 +4,6 @@ import ru.spbstu.amd.learnbraille.database.Lesson
 
 // TODO fill lessons
 val PREPOPULATE_LESSONS = listOf(
-    Lesson(id = 1, name = "Вводное введение"),
-    Lesson(id = 2, name = "Подводное подведение")
+    Lesson(id = 1, name = "Знакомство с шеститочием"),
+    Lesson(id = 2, name = "Русские буквы А, Б,Ц")
 )
