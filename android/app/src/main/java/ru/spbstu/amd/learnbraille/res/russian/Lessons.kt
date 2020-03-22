214917965a1b10d6767e7677f8357ce948745293
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-03-22 12:03:06 +0300

    Update Lessons.kt

--- android/app/src/main/java/ru/spbstu/amd/learnbraille/res/russian/Lessons.kt
+++ android/app/src/main/java/ru/spbstu/amd/learnbraille/res/russian/Lessons.kt
@@ -5,5 +5,5 @@ import ru.spbstu.amd.learnbraille.database.Lesson
 // TODO fill lessons
 val PREPOPULATE_LESSONS = listOf(
     Lesson(id = 1, name = "Знакомство с шеститочием"),
-    Lesson(id = 2, name = "Русские буквы А, Б,Ц")
+    Lesson(id = 2, name = "Русские буквы А, Б, Ц")
 )
