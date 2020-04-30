718bebe668989f53efb913d5c0431e9f64f324b4
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-04-30 22:14:27 +0300

    5.4 fix help crash when accessibility disabled

--- android/app/src/main/java/com/github/braillesystems/learnbraille/util/AccessibilityHelper.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/util/AccessibilityHelper.kt
@@ -2,6 +2,7 @@ package com.github.braillesystems.learnbraille.util
 
 import android.content.Context
 import android.content.Context.ACCESSIBILITY_SERVICE
+import android.provider.Settings
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityEvent.TYPE_ANNOUNCEMENT
 import android.view.accessibility.AccessibilityManager
@@ -11,16 +12,22 @@ class AccessibilityHelper {
     companion object {
         @JvmStatic
         fun announceForAccessibility(context: Context?, announcement: String) {
-            context!!
-                .getSystemService(ACCESSIBILITY_SERVICE)
+
+            val accessibilityEnabled: Int = Settings.Secure.getInt(
+                context?.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED
+            )
+            if (accessibilityEnabled != 1){
+                return
+            }
+            context?.getSystemService(ACCESSIBILITY_SERVICE)
                 .let { it as AccessibilityManager }
                 .let { manager ->
                     AccessibilityEvent
                         .obtain()
                         .apply {
                             eventType = TYPE_ANNOUNCEMENT
-                            className = context.javaClass.name
-                            packageName = context.packageName
+                            className = context?.javaClass?.name
+                            packageName = context?.packageName
                             val re = Regex("\\<[^>]*>") // remove HTML markup
                             text.add(re.replace(announcement, ""))
                         }
