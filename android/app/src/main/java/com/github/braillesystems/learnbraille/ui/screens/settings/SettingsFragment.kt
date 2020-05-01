40aff9b5e966ecb02d7d0dc712bb043050783903
kaustika <snufkinson@yandex.ru>
2020-05-02 00:17:43 +0300

    5.9 add basic settings UI

---
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.github.braillesystems.learnbraille.ui.screens.settings
+
+import android.os.Bundle
+import com.github.braillesystems.learnbraille.R
+
+class SettingsFragment : androidx.preference.PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_hierarchy, rootKey)
+    }
+}
