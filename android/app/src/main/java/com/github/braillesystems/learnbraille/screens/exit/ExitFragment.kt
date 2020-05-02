d79161a917913e10e8ec1eb38da3d29ad5638865
Мария <masha-bavikina@yandex.ru>
2020-05-02 22:24:48 +0300

    5.10 replace google now with SpeechRecognizer

--- android/app/src/main/java/com/github/braillesystems/learnbraille/screens/exit/ExitFragment.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/screens/exit/ExitFragment.kt
@@ -1,28 +1,30 @@
 package com.github.braillesystems.learnbraille.screens.exit
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.github.braillesystems.learnbraille.R
 import com.github.braillesystems.learnbraille.databinding.FragmentExitBinding
-import com.github.braillesystems.learnbraille.util.application
 import com.github.braillesystems.learnbraille.util.updateTitle
 import java.util.*
 import kotlin.system.exitProcess
 
-class ExitFragment : Fragment() {
+open class ExitFragment : Fragment() {
 
     private lateinit var mTTs:TextToSpeech
+    private var mostRecentUtteranceID: String? = null
 
-    private val REQUEST_CODE_SPEECH_INPUT = 100
+    private var mSpeechRecognizer: SpeechRecognizer? = null
+    private var mSpeechRecognizerIntent: Intent? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,12 +40,45 @@ class ExitFragment : Fragment() {
         updateTitle(getString(R.string.exit_question))
 
         mTTs = TextToSpeech(context, TextToSpeech.OnInitListener {status ->
-            if (status != TextToSpeech.ERROR) {
+            if (status == TextToSpeech.SUCCESS) {
                 mTTs.language = Locale.UK
+                mostRecentUtteranceID = (Random().nextInt() % 9999999).toString() + ""
+
+                val params: MutableMap<String, String?> = HashMap()
+                params[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = mostRecentUtteranceID
+                mTTs.speak(getString(R.string.exit_question), TextToSpeech.QUEUE_FLUSH,
+                    params as HashMap<String, String>?
+                )
+                mTTs.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+                    override fun onDone(utteranceId: String) {
+                        if (utteranceId != mostRecentUtteranceID) {
+                            return
+                        }
+                        activity?.runOnUiThread {
+                            if (mSpeechRecognizer != null) {
+                                mSpeechRecognizer!!.startListening(mSpeechRecognizerIntent)
+                            };
+                        }
+                    }
+                    override fun onError(utteranceId: String) {}
+                    override fun onStart(utteranceId: String) {}
+                })
+
             }
         })
 
-        mTTs.speak(getString(R.string.exit_question), TextToSpeech.QUEUE_FLUSH, null)
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(context)
+        mSpeechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        mSpeechRecognizerIntent!!.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        mSpeechRecognizerIntent!!.putExtra(
+            RecognizerIntent.EXTRA_CALLING_PACKAGE,
+            context?.packageName
+        )
+        mSpeechRecognizer?.setRecognitionListener(SpeechRecognitionListener())
+
 
         exitButton.setOnClickListener {
             exitProcess(0)
@@ -53,32 +88,31 @@ class ExitFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_global_menuFragment)
         )
 
-        speak()
     }.root
 
-
-    private fun speak(){
-        val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.exit_question))
-        startActivityForResult(mIntent, REQUEST_CODE_SPEECH_INPUT)
+    override fun onDestroy() {
+        super.onDestroy()
+        mSpeechRecognizer?.destroy()
     }
 
-    override fun OnActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        //super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode){
-            REQUEST_CODE_SPEECH_INPUT -> {
-                if (resultCode == Activity.RESULT_OK && null != data) {
-                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                    if (result?.get(0)?.equals(getString(R.string.exit_voice_yes))!!){
-                        exitProcess(0)
-                    } else if (result.get(0)?.equals(getString(R.string.exit_voice_no))!!){
-                        Navigation.createNavigateOnClickListener(R.id.action_global_menuFragment)
-                    }
-                }
+    protected class SpeechRecognitionListener : RecognitionListener {
+        override fun onBeginningOfSpeech() {}
+        override fun onBufferReceived(buffer: ByteArray) {}
+        override fun onEndOfSpeech() {}
+        override fun onError(error: Int) {}
+        override fun onEvent(eventType: Int, params: Bundle) {}
+        override fun onPartialResults(partialResults: Bundle) {}
+        override fun onReadyForSpeech(params: Bundle) {}
+        override fun onRmsChanged(rmsdB: Float) {}
+
+        override fun onResults(results: Bundle) {
+            val matches =
+                results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            if (matches!![0].equals("да")){
+                exitProcess(0)
+            }
+            if (matches[0].equals("нет")){
+                Navigation.createNavigateOnClickListener(R.id.action_global_menuFragment)
             }
         }
     }
