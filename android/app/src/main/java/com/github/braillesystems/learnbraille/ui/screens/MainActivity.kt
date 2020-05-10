797029d07d93c060e16616d5b93fc90d5a54c0fc
kaustika <snufkinson@yandex.ru>
2020-05-10 19:42:13 +0300

    6.7 add overflow menu UI

--- android/app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/MainActivity.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.braillesystems.learnbraille.ui.screens
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -10,11 +11,13 @@ import androidx.navigation.ui.NavigationUI
 import com.github.braillesystems.learnbraille.R
 import com.github.braillesystems.learnbraille.databinding.ActivityMainBinding
 import com.github.braillesystems.learnbraille.ui.serial.UsbParser
+import com.google.android.material.appbar.MaterialToolbar
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var toolbar: MaterialToolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +25,24 @@ class MainActivity : AppCompatActivity() {
         Timber.i("onCreate")
 
         DataBindingUtil.setContentView<ActivityMainBinding>(
-            this,
-            R.layout.activity_main
+                this,
+                R.layout.activity_main
         )
 
+        toolbar = findViewById(R.id.topAppBar)
+        setSupportActionBar(toolbar)
         navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
         UsbParser.init(this)
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 
     override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 }
