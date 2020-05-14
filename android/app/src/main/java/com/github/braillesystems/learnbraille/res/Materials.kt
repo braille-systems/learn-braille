450603723b4bde6101355db80ffc92dd4b44f238
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-05-14 15:16:37 +0300

    5.9 add lessons until 12

--- android/app/src/main/java/com/github/braillesystems/learnbraille/res/Materials.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/res/Materials.kt
@@ -55,6 +55,7 @@ val ruSymbols by symbols(SymbolType.ru) {
 
 val specialSymbols by symbols(SymbolType.special){
     symbol(symbol = ']', brailleDots = BrailleDots(E, E, F, F, F, F)) // цифровой знак // number
+    symbol(symbol = ',', brailleDots = BrailleDots(E, F, E, E, E, E)) // Знак препинания 'Запятая'
 }
 
 val uebDigits by symbols(SymbolType.digit){
