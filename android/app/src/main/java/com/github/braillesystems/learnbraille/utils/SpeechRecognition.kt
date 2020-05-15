8bcb24e942c9be5f3ab450a2ae22cccfa8dc8bdb
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-05-15 23:29:41 +0400

    Refactor checked access

--- android/app/src/main/java/com/github/braillesystems/learnbraille/utils/SpeechRecognition.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/utils/SpeechRecognition.kt
@@ -21,7 +21,7 @@ import kotlin.system.exitProcess
 
 class SpeechRecognition(
     private val fragment: Fragment,
-    preferenceRepository: PreferenceRepository
+    private val preferenceRepository: PreferenceRepository
 ) {
 
     private var mostRecentUtteranceID: String? = null
@@ -30,7 +30,7 @@ class SpeechRecognition(
     private var mSpeechRecognizerIntent: Intent? = null
 
     init {
-        if (preferenceRepository.speechRecognitionEnabled) {
+        executeIf(preferenceRepository.speechRecognitionEnabled) {
             try {
                 initialize()
             } catch (e: Throwable) {
@@ -84,7 +84,7 @@ class SpeechRecognition(
         mSpeechRecognizer?.setRecognitionListener(SpeechRecognitionListener(fragment))
     }
 
-    fun onDestroy() {
+    fun onDestroy() = executeIf(preferenceRepository.speechRecognitionEnabled) {
         try {
             mSpeechRecognizer?.destroy()
         } catch (e: Throwable) {
@@ -94,6 +94,7 @@ class SpeechRecognition(
 }
 
 private class SpeechRecognitionListener(fragment: Fragment) : RecognitionListener {
+
     override fun onBeginningOfSpeech() {}
     override fun onBufferReceived(buffer: ByteArray) {}
     override fun onEndOfSpeech() {}
--- android/app/src/main/java/com/github/braillesystems/learnbraille/utils/Utils.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/utils/Utils.kt
@@ -26,17 +26,17 @@ val Fragment.application: LearnBrailleApplication
 
 fun scope(job: Job = Job()) = CoroutineScope(Dispatchers.Main + job)
 
-fun Vibrator?.checkedBuzz(pattern: BuzzPattern, preferenceRepository: PreferenceRepository) {
-    if (preferenceRepository.buzzEnabled) buzz(pattern)
-}
+fun Vibrator?.checkedBuzz(pattern: BuzzPattern, preferenceRepository: PreferenceRepository) =
+    executeIf(preferenceRepository.buzzEnabled) {
+        buzz(pattern)
+    }
 
-fun checkedToast(msg: String, context: Context?, preferenceRepository: PreferenceRepository) {
-    if (preferenceRepository.toastsEnabled) {
+fun checkedToast(msg: String, context: Context?, preferenceRepository: PreferenceRepository) =
+    executeIf(preferenceRepository.toastsEnabled) {
         Toast.makeText(
             context, msg, preferenceRepository.toastDuration
         ).show()
     }
-}
 
 fun Fragment.checkedToast(msg: String, preferenceRepository: PreferenceRepository = get()) =
     checkedToast(msg, context, preferenceRepository)
@@ -49,12 +49,7 @@ fun Fragment.toast(msg: String, preferenceRepository: PreferenceRepository = get
 fun Context.announceByAccessibility(
     announcement: String,
     preferenceRepository: PreferenceRepository
-) {
-    if (!preferenceRepository.announcementsEnabled) {
-        Timber.i("Announcements disabled")
-        return
-    }
-
+) = executeIf(preferenceRepository.announcementsEnabled) {
     val manager = accessibilityManager ?: return
     val event = AccessibilityEvent.obtain().apply {
         eventType = AccessibilityEvent.TYPE_ANNOUNCEMENT
