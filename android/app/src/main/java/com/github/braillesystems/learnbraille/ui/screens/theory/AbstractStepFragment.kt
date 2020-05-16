6b6f63316f836b6226618ec3d3037e9fd25be1cc
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-05-17 01:49:18 +0400

    6.11 override menu for steps

--- android/app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/theory/AbstractStepFragment.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/theory/AbstractStepFragment.kt
@@ -1,8 +1,12 @@
 package com.github.braillesystems.learnbraille.ui.screens.theory
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.github.braillesystems.learnbraille.R
 import com.github.braillesystems.learnbraille.ui.screens.AbstractFragmentWithHelp
 import com.github.braillesystems.learnbraille.ui.screens.HelpMsgId
+import com.github.braillesystems.learnbraille.utils.toast
 import com.github.braillesystems.learnbraille.utils.updateTitle
 
 
@@ -16,6 +20,19 @@ abstract class AbstractStepFragment(helpMsgId: HelpMsgId) : AbstractFragmentWith
 
     protected fun updateStepTitle(lessonId: Long, stepId: Long, msgId: Int) {
         updateTitle("$lessonId.$stepId ${getString(msgId)}")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.steps_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = false.also {
+        when (item.itemId) {
+            R.id.help -> toast("AAAAAA")
+            R.id.lessons_list -> toast("BBBBBB")
+            R.id.current_course_pos -> toast("CCCCC")
+            else -> TODO()
+        }
     }
 
     companion object {
