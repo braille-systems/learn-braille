85a9dd1ebf73be4f8a307c88af3165744d0c0e2d
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-05-22 11:32:21 +0400

    Fix prefference default values consistency

--- android/app/src/main/java/com/github/braillesystems/learnbraille/data/repository/PreferenceRepository.kt
+++ android/app/src/main/java/com/github/braillesystems/learnbraille/data/repository/PreferenceRepository.kt
@@ -18,7 +18,6 @@ interface PreferenceRepository {
     val brailleTrainerEnabled: Boolean get() = false // Uncomment in android manifest when set true
     val speechRecognitionEnabled: Boolean
     val golubinaBookStepsEnabled: Boolean
-    val practiceUseMaterialsPassedInCourse: Boolean
     val traverseDotsInEnumerationOrder: Boolean
     val inputOnFlyCheck: Boolean
     val additionalAnnouncementsEnabled: Boolean
@@ -36,6 +35,10 @@ interface PreferenceRepository {
 
 interface MutablePreferenceRepository : PreferenceRepository
 
+/**
+ * Keep default values same as in `settings_hierarchy`.
+ * Values here will be used before the user first time visit to the preferences.
+ */
 class PreferenceRepositoryImpl(
     private val context: Context,
     private val userDao: UserDao
@@ -58,7 +61,7 @@ class PreferenceRepositoryImpl(
     override val speechRecognitionEnabled: Boolean by logged {
         context.preferences.getBoolean(
             context.getString(R.string.preference_speech_recognition_enabled),
-            false
+            false // To enable, set to `true` and uncomment permission in AndroidManifest
         )
     }
 
@@ -69,18 +72,10 @@ class PreferenceRepositoryImpl(
         )
     }
 
-    override val practiceUseMaterialsPassedInCourse: Boolean by logged {
-        context.preferences.getBoolean(
-            context.getString(R.string.preference_practice_use_passed_material),
-            // to enable recognition, set to `true` and uncomment permission in AndroidManifest
-            false // TODO
-        )
-    }
-
     override val traverseDotsInEnumerationOrder: Boolean by logged {
         context.preferences.getBoolean(
             context.getString(R.string.preference_traverse_dots_in_enumeration_order),
-            false
+            true
         )
     }
 
@@ -94,21 +89,21 @@ class PreferenceRepositoryImpl(
     override val additionalAnnouncementsEnabled: Boolean by logged {
         context.preferences.getBoolean(
             context.getString(R.string.preference_enable_additional_announcements),
-            false
+            true
         )
     }
 
     override val practiceUseOnlyKnownMaterials: Boolean by logged {
         context.preferences.getBoolean(
-            context.getString(R.string.preference_practice_use_only_seen_materials),
-            false
+            context.getString(R.string.preference_practice_use_only_known_materials),
+            true
         )
     }
 
     override val additionalExitButtonsEnabled: Boolean by logged {
         context.preferences.getBoolean(
             context.getString(R.string.preference_additional_exit_buttons_enabled),
-            false
+            true
         )
     }
 
