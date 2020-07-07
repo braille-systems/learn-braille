4f8c6bedbbad80f3ebd069e1206c5da56cf9b359
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-07-07 12:37:22 +0400

    #202 fix hint text in steps with dots

--- app/src/main/java/com/github/braillesystems/learnbraille/ui/Messages.kt
+++ app/src/main/java/com/github/braillesystems/learnbraille/ui/Messages.kt
@@ -3,12 +3,12 @@ package com.github.braillesystems.learnbraille.ui
 import android.content.Context
 import androidx.fragment.app.Fragment
 import com.github.braillesystems.learnbraille.R
+import com.github.braillesystems.learnbraille.data.entities.BrailleDot
 import com.github.braillesystems.learnbraille.data.entities.BrailleDots
-import com.github.braillesystems.learnbraille.data.entities.spelling
+import com.github.braillesystems.learnbraille.data.entities.list
 import com.github.braillesystems.learnbraille.res.inputSymbolPrintRules
 import com.github.braillesystems.learnbraille.res.showSymbolPrintRules
-import com.github.braillesystems.learnbraille.utils.checkedToast
-import com.github.braillesystems.learnbraille.utils.toast
+import com.github.braillesystems.learnbraille.utils.*
 import timber.log.Timber
 
 enum class PrintMode {
@@ -24,8 +24,27 @@ fun Fragment.showIncorrectToast(c: Char? = null): Unit =
                 printString(c, PrintMode.INPUT).orEmpty()
     )
 
+val Context.dotsHintRules by lazyWithContext<Context, List<String>> {
+    listOf(
+        getString(R.string.input_dots_hint_1),
+        getString(R.string.input_dots_hint_2),
+        getString(R.string.input_dots_hint_3),
+        getString(R.string.input_dots_hint_4),
+        getString(R.string.input_dots_hint_5),
+        getString(R.string.input_dots_hint_6)
+    )
+}
+
 fun Fragment.showHintDotsToast(expectedDots: BrailleDots) =
-    checkedToast(getString(R.string.input_dots_hint_template).format(expectedDots.spelling))
+    getString(R.string.input_dots_hint_template)
+        .format(
+            expectedDots.list
+                .mapIndexedNotNull { index, brailleDot ->
+                    if (brailleDot == BrailleDot.F) application.dotsHintRules[index] else null
+                }
+                .joinToString(separator = ", ")
+        )
+        .side { checkedToast(it) }
 
 fun Context.printString(c: Char, mode: PrintMode): String? =
     when (mode) {
