033115f190de2d0974abb391d1e0e0d6045857fa
zuevval <valera.zuev.zva@gmail.com>
2020-09-08 14:53:59 +0300

    #224 add greek and latin symbols

--- app/src/main/java/com/github/braillesystems/learnbraille/res/_DSL.kt
+++ app/src/main/java/com/github/braillesystems/learnbraille/res/_DSL.kt
@@ -2,8 +2,15 @@ package com.github.braillesystems.learnbraille.res
 
 import com.github.braillesystems.learnbraille.data.dsl.StepsBuilder
 import com.github.braillesystems.learnbraille.data.entities.Input
+import com.github.braillesystems.learnbraille.data.entities.Show
 
 fun StepsBuilder.inputChars(chars: String): Unit =
     chars
         .map(Char::toUpperCase)
         .forEach { c -> +Input(content.symbols.getValue(c)) }
+
+fun StepsBuilder.showAndInputChars(chars: String): Unit =
+    chars.map(Char::toUpperCase).forEach {
+        +Show(content.symbols.getValue(it))
+        +Input(content.symbols.getValue(it))
+    }
