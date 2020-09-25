12c645423ea44abbf3824276ee5fbd208e8234e1
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-09-25 21:13:39 +0300

    Display last lesson bold in lessons list

--- app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/theory/lessons/LessonsListFragment.kt
+++ app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/theory/lessons/LessonsListFragment.kt
@@ -1,6 +1,7 @@
 package com.github.braillesystems.learnbraille.ui.screens.theory.lessons
 
 import android.annotation.SuppressLint
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -43,6 +44,7 @@ class LessonsListFragment : Fragment() {
         lifecycleScope.launch {
             val curr = theoryRepository.currentStep(COURSE.id)
             val lessons = theoryRepository.allCourseLessons(COURSE.id)
+            val last = theoryRepository.lastCourseStep(curr.courseId)
             val activeListener = object : LessonItemListener {
                 override fun onClick(item: Lesson) = toLastLessonStep(COURSE.id, item.id)
             }
@@ -56,6 +58,11 @@ class LessonsListFragment : Fragment() {
             val adapter = LessonsListAdapter(lessons) { item ->
                 lesson = item
                 lessonName.text = "${item.id}. ${item.name}"
+                lessonName.setTypeface(
+                    lessonName.typeface,
+                    if (item.id == last.lessonId) Typeface.BOLD
+                    else Typeface.NORMAL
+                )
                 if (item.id <= curr.lessonId) {
                     clickListener = activeListener
                     lessonName.setTextColor(
